7668c2702afd264daaf1546ae6dcea29693f4a2f
Amir <amir.layes@gmail.com>
2018-01-11 22:55:24 +0100

    Ajout constructeur pour TU. (A revoir) Suppression imports inutiles.

--- src/main/java/com/amir/domain/Document.java
+++ src/main/java/com/amir/domain/Document.java
@@ -1,20 +1,11 @@
 package com.amir.domain;
 
-import java.util.Map;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.NamedQuery;
-import org.hibernate.annotations.Type;
-import org.hibernate.annotations.TypeDef;
-
-import com.amir.hql.JsonDataUserType;
 
 @Entity
 @Table (name="document")
@@ -31,7 +22,14 @@ public class Document {
 //    @Type(type="text")
     @Column(length=10485760)
     private String file;
-    
+    public Document() {
+	}
+    public Document(Long id, String name, String file) {
+    	super();
+		this.id = id;
+		this.docName = docName;
+		this.file = file;
+	}
     public Long getId() {
         return id;
     }
