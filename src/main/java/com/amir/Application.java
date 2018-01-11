cac0c8d6b55e5d8abac5c959977ae6017965d9d4
Amir <amir.layes@gmail.com>
2018-01-11 22:45:03 +0100

    Config multipart remise dans application.properties. Enlever @EnableJpaRepositories (ne sert pas pour l'instant).

--- src/main/java/com/amir/Application.java
+++ src/main/java/com/amir/Application.java
@@ -1,37 +1,13 @@
 package com.amir;
 
-import javax.servlet.MultipartConfigElement;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
 public class Application extends SpringBootServletInitializer {
-
-    @Bean
-    public MultipartConfigElement multipartConfigElement() {
-		MultipartConfigFactory factory = new MultipartConfigFactory();
-        factory.setMaxFileSize("5120KB");
-        factory.setMaxRequestSize("5120KB");
-        return factory.createMultipartConfig();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(applicationClass);
-    }
-
-    private static Class<Application> applicationClass = Application.class;
 }
