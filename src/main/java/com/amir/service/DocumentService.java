58e17b20f84be42f5d9149b3e76bfc7f8c2f2f10
Amir <amir.layes@gmail.com>
2018-01-11 23:02:56 +0100

    Lignes inutiles supprimées

--- src/main/java/com/amir/service/DocumentService.java
+++ src/main/java/com/amir/service/DocumentService.java
@@ -2,10 +2,6 @@ package com.amir.service;
 
 import java.io.IOException;
 import java.util.List;
-
-import org.apache.tika.exception.TikaException;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.xml.sax.SAXException;
