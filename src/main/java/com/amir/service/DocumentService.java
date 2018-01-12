04ce443047ff18d59bbf763cba0cf320ed00d908
Amir <amir.layes@gmail.com>
2018-01-12 10:05:19 +0100

    regression : remise import TikaException.

--- src/main/java/com/amir/service/DocumentService.java
+++ src/main/java/com/amir/service/DocumentService.java
@@ -2,6 +2,8 @@ package com.amir.service;
 
 import java.io.IOException;
 import java.util.List;
+
+import org.apache.tika.exception.TikaException;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.xml.sax.SAXException;
