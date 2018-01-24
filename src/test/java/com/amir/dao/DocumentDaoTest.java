815d4b501d1a94faaf1e51f958607cc33d8ead92
Amir <amir.layes@gmail.com>
2018-01-25 00:18:54 +0100

    Repository JPA tests... In-memory Database configuration.. Could be changed to springboot included hsql database ..

--- src/test/java/com/amir/dao/DocumentDaoTest.java
+++ src/test/java/com/amir/dao/DocumentDaoTest.java
@@ -3,18 +3,20 @@ package com.amir.dao;
 import static org.junit.Assert.*;
 
 import java.util.List;
-
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import com.amir.domain.Document;
 
+@RunWith(SpringRunner.class)
 @DataJpaTest
 public class DocumentDaoTest {
-	 @Autowired
-	 private TestEntityManager entityManager;
+	 //@Autowired (removed because Springboot provides autoconfiguredTestEntityManager
+	 //private TestEntityManager entityManager;
 	 
 	 @Autowired
 	 private DocumentDao DocumentDao;
@@ -22,8 +24,7 @@ public class DocumentDaoTest {
 	 @Test
 	 public void whenFindByFileContains_thenReturnListDocsContainingInputText() {
 	     Document doc = new Document(1L, "doc1.txt", "document test Content");
-	     entityManager.persist(doc);
-	     entityManager.flush();
+	     DocumentDao.save(doc);
 	     
 	     List<Document> found = DocumentDao.findByFileContainsAllIgnoreCase("test Content");
 	  
