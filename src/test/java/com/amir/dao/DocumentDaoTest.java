810fee5bccfe2823d1defff5835c56c3e23a8ec9
Amir <amir.layes@gmail.com>
2018-01-25 10:50:22 +0100

    + Remove not used import. + Add more test use cases.

--- src/test/java/com/amir/dao/DocumentDaoTest.java
+++ src/test/java/com/amir/dao/DocumentDaoTest.java
@@ -8,8 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-
+import static org.assertj.core.api.Assertions.*;
 import com.amir.domain.Document;
 
 @RunWith(SpringRunner.class)
@@ -22,6 +21,14 @@ public class DocumentDaoTest {
 	 private DocumentDao DocumentDao;
 	 
 	 @Test
+	public void should_store_a_document() {
+		Document doc = DocumentDao.save(new Document(1L, "doc1.txt", "document test Content"));
+ 
+		assertThat(doc).hasFieldOrPropertyWithValue("docName", "doc1.txt");
+		assertThat(doc).hasFieldOrPropertyWithValue("file", "document test Content");
+	}
+	 
+	 @Test
 	 public void whenFindByFileContains_thenReturnListDocsContainingInputText() {
 	     Document doc = new Document(1L, "doc1.txt", "document test Content");
 	     DocumentDao.save(doc);
@@ -29,5 +36,25 @@ public class DocumentDaoTest {
 	     List<Document> found = DocumentDao.findByFileContainsAllIgnoreCase("test Content");
 	  
 	     assertEquals(found.get(0).getDocName(), doc.getDocName());
+	 }
+	 
+	 @Test
+	 public void should_delete_all_documents(){
+		 DocumentDao.save(new Document(1L, "doc1.txt", "document test Content1"));
+		 DocumentDao.save(new Document(1L, "doc2.txt", "document test Content2"));
+		 
+		 DocumentDao.deleteAll();
+		 
+		 assertTrue(((List<Document>) DocumentDao.findAll()).isEmpty());
+	 }
+	 
+	 @Test
+	 public void should_find_all_documents(){
+		 Document doc1 = DocumentDao.save(new Document(1L, "doc1.txt", "document test Content1"));
+		 Document doc2 = DocumentDao.save(new Document(1L, "doc2.txt", "document test Content2"));
+		 
+		 Iterable<Document> docs = DocumentDao.findAll();
+		 
+		 assertThat(docs).hasSize(2).contains(doc1, doc2);
 	 }
 }
