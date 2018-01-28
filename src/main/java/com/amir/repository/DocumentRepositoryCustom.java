b4143b39330d3a9f3b3b8979629fcb71426d3e85
Amir <amir.layes@gmail.com>
2018-01-28 23:52:32 +0100

    same

--- src/main/java/com/amir/repository/DocumentRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.amir.repository;
-
-import java.util.List;
-
-import com.amir.domain.Document;
-public interface DocumentRepositoryCustom {
-	
-	List<Document> fulltextSearch(String name);
-}
