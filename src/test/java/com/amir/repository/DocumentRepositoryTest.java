294587dbd7045814c41e9bcff4ca994f6623d8c7
Amir <amir.layes@gmail.com>
2018-01-30 18:07:45 +0100

    Replace Dao by Repository

--- src/test/java/com/amir/repository/DocumentRepositoryTest.java
+++ src/test/java/com/amir/repository/DocumentRepositoryTest.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import static org.assertj.core.api.Assertions.*;
 import com.amir.domain.Document;
-import com.amir.domain.DocumentDao;
+import com.amir.repository.DocumentRepository;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -19,10 +19,10 @@ public class DocumentRepositoryTest {
 	 //private TestEntityManager entityManager;
 	 
 	 @Autowired
-	 private DocumentDao DocumentDao;
+	 private DocumentRepository DocumentRepository;
 	 @Test
 	public void should_store_a_document() {
-		Document doc = DocumentDao.save(new Document(1L, "doc1.txt", "document test Content"));
+		Document doc = DocumentRepository.save(new Document(1L, "doc1.txt", "document test Content"));
  
 		assertThat(doc).hasFieldOrPropertyWithValue("docName", "doc1.txt");
 		assertThat(doc).hasFieldOrPropertyWithValue("file", "document test Content");
@@ -31,29 +31,29 @@ public class DocumentRepositoryTest {
 	 @Test
 	 public void whenFindByFileContains_thenReturnListDocsContainingInputText() {
 	     Document doc = new Document(1L, "doc1.txt", "document test Content");
-	     DocumentDao.save(doc);
+	     DocumentRepository.save(doc);
 	     
-	     List<Document> found = DocumentDao.findByFileContainsAllIgnoreCase("test Content");
+	     List<Document> found = DocumentRepository.findByFileContainsAllIgnoreCase("test Content");
 	  
 	     assertEquals(found.get(0).getDocName(), doc.getDocName());
 	 }
 	 
 	 @Test
 	 public void should_delete_all_documents(){
-		 DocumentDao.save(new Document(1L, "doc1.txt", "document test Content1"));
-		 DocumentDao.save(new Document(1L, "doc2.txt", "document test Content2"));
+		 DocumentRepository.save(new Document(1L, "doc1.txt", "document test Content1"));
+		 DocumentRepository.save(new Document(1L, "doc2.txt", "document test Content2"));
 		 
-		 DocumentDao.deleteAll();
+		 DocumentRepository.deleteAll();
 		 
-		 assertTrue(((List<Document>) DocumentDao.findAll()).isEmpty());
+		 assertTrue(((List<Document>) DocumentRepository.findAll()).isEmpty());
 	 }
 	 
 	 @Test
 	 public void should_find_all_documents(){
-		 Document doc1 = DocumentDao.save(new Document(1L, "doc1.txt", "document test Content1"));
-		 Document doc2 = DocumentDao.save(new Document(1L, "doc2.txt", "document test Content2"));
+		 Document doc1 = DocumentRepository.save(new Document(1L, "doc1.txt", "document test Content1"));
+		 Document doc2 = DocumentRepository.save(new Document(1L, "doc2.txt", "document test Content2"));
 		 
-		 Iterable<Document> docs = DocumentDao.findAll();
+		 Iterable<Document> docs = DocumentRepository.findAll();
 		 
 		 assertThat(docs).hasSize(2).contains(doc1, doc2);
 	 }
