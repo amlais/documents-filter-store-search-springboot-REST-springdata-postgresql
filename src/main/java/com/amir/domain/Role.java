6b181bedf1e05065f2f43aa9af5233e687547f58
Amir <amir.layes@gmail.com>
2018-02-01 22:34:49 +0100

    Role add

---
+++ src/main/java/com/amir/domain/Role.java
@@ -0,0 +1,29 @@
+package com.amir.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+import javax.persistence.Id;
+
+@Entity
+
+public class Role {
+	 /*@GenericGenerator(
+	        name = "wikiSequenceGenerator",
+	        strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+	        parameters = {
+	                @Parameter(name = "sequence_name", value = "WIKI_SEQUENCE"),
+	                @Parameter(name = "initial_value", value = "1"),
+	                @Parameter(name = "increment_size", value = "1")
+	        }
+	)*/
+	@Id
+	@GeneratedValue
+	private Long id;
+	String name;
+	Role(){}
+	
+	public Role(String name){ this.name=name;}
+	public String getName(){ return name;}
+	public void setName(String name){ this.name=name;}
+}
