ff4cdb7c4e1f64424b0e77c537c247b876810d46
Amir <amir.layes@gmail.com>
2018-02-01 18:44:00 +0100

    Security jdbc (minimal config) Basic authentification customisation

---
+++ src/main/java/com/amir/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.amir.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	DataSource dataSource;
+
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth)
+		throws Exception {
+
+		auth.jdbcAuthentication().dataSource(dataSource)
+			.passwordEncoder(passwordEncoder())
+			.usersByUsernameQuery("select username,password,enabled"+
+	                 "from users where username = ?")
+			.authoritiesByUsernameQuery("select username, role from"+ 
+	                 "roles where username = ?");
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		PasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
+	}
+}
