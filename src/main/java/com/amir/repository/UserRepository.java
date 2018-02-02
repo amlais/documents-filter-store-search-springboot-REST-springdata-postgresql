17cdf06665daab905a19b40eae153603f6f4689b
Amir <amir.layes@gmail.com>
2018-02-02 16:58:01 +0100

    To be autowired into CustomUserDetailsService

--- src/main/java/com/amir/repository/UserRepository.java
+++ src/main/java/com/amir/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.amir.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.amir.domain.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 	User findByUsername(String username);
 }
