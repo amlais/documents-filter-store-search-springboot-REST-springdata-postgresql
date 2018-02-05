38aa41f1a7ee6f2f517356c74ccb24f9d513c279
Amir <amir.layes@gmail.com>
2018-02-05 13:14:04 +0100

    Repository

--- src/main/java/com/amir/repository/ElasticDocumentRepository.java
+++ src/main/java/com/amir/repository/ElasticDocumentRepository.java
@@ -1,17 +1,16 @@
 package com.amir.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
 import com.amir.domain.ElasticDocument;
 
 import java.util.List;
 
+@Repository
 public interface ElasticDocumentRepository extends ElasticsearchRepository<ElasticDocument, String> {
 
-    //Page<ElasticDocument> findByFile(String searchQuery, Pageable pageable);
-
+    //will be replaced by search(org.elasticsearch.index.query.QueryBuilder q) elastic java query API
     List<ElasticDocument> findByFileContainingAllIgnoreCase(String searchQuery);
 
 }
