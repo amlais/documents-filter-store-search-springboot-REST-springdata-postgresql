8b022b3b5e423608d7aaef0bfcbb2ad2aa111298
Amir <amir.layes@gmail.com>
2018-02-06 18:24:17 +0100

    Unused imports

--- src/main/java/com/amir/config/AuthorizationServerConfig.java
+++ src/main/java/com/amir/config/AuthorizationServerConfig.java
@@ -1,12 +1,9 @@
 package com.amir.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
