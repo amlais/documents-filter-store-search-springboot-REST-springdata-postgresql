f640a316361a0562981a24ee84a4c60239278302
Amir <amir.layes@gmail.com>
2018-02-07 18:10:50 +0100

    used to check passsword

--- src/main/java/com/amir/service/CustomUserDetailsService.java
+++ src/main/java/com/amir/service/CustomUserDetailsService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.amir.domain.User;
@@ -19,6 +21,9 @@ public class CustomUserDetailsService implements UserDetailsService{
 	UserRepository userRepository;
 	@Override
 	public UserDetails loadUserByUsername(String username) {
+		String password = "p4ssword";
+		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		System.out.println(passwordEncoder.encode(password));
 		Optional<User> optionalUsers = userRepository.findByUsername(username);
 		optionalUsers
 			.orElseThrow(() -> new UsernameNotFoundException("Username notfound"));
