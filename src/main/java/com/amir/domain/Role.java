715c52aa65e0ab3726b443f91de317c8c6097af6
Amir <amir.layes@gmail.com>
2018-02-08 19:01:25 +0100

    delete comment

--- src/main/java/com/amir/domain/Role.java
+++ src/main/java/com/amir/domain/Role.java
@@ -1,5 +1,6 @@
 package com.amir.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,15 +8,6 @@ import javax.persistence.Id;
 
 @Entity
 public class Role {
-	 /*@GenericGenerator(
-	        name = "wikiSequenceGenerator",
-	        strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
-	        parameters = {
-	                @Parameter(name = "sequence_name", value = "WIKI_SEQUENCE"),
-	                @Parameter(name = "initial_value", value = "1"),
-	                @Parameter(name = "increment_size", value = "1")
-	        }
-	)*/
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Long id;
