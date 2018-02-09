0cff44adf1977fbda75c1edd7b052e9206319653
Amir <amir.layes@gmail.com>
2018-02-09 13:30:17 +0100

    authorizationserver v1

--- src/main/java/com/amir/config/AuthorizationServerConfig.java
+++ src/main/java/com/amir/config/AuthorizationServerConfig.java
@@ -3,17 +3,14 @@ package com.amir.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-//import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-//
+
 @Configuration
 @EnableAuthorizationServer
-//extends AuthorizationServerConfigurerAdapter
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
 	
 	@Autowired
@@ -30,8 +27,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.inMemory()
 			.withClient("upload-client")
 			.authorizedGrantTypes("client-credentials", "password")
-			.authorities("ROLE_ADMIN", "ROLE_USER")
-			.scopes("read", "write")
+			.authorities("ADMIN", "USER")
+			.scopes("read", "write","trust")
 			.resourceIds("oauth2-resource")
 			.accessTokenValiditySeconds(5000)
 			.secret("uploadpass");
