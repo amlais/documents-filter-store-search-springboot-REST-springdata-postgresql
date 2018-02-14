6c87220758709a07f82a7fdc3820adf2b4e359cb
Amir <amir.layes@gmail.com>
2018-02-14 17:43:27 +0100

    Add findOne and delete methods.

--- src/main/java/com/amir/repository/ElasticDocumentRepository.java
+++ src/main/java/com/amir/repository/ElasticDocumentRepository.java
@@ -2,15 +2,18 @@ package com.amir.repository;
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.amir.domain.ElasticDocument;
 
 import java.util.List;
 
 @Repository
+@Transactional(readOnly=true)
 public interface ElasticDocumentRepository extends ElasticsearchRepository<ElasticDocument, String> {
 
     //will be replaced by search(org.elasticsearch.index.query.QueryBuilder q) elastic java query API
     List<ElasticDocument> findByFileContainingAllIgnoreCase(String searchQuery);
-
+    ElasticDocument findOne(String id);
+    void delete(ElasticDocument ed);
 }
