0b97139b335b29eefc8813fc45e0ba0e09a98c56
Amir <amir.layes@gmail.com>
2018-02-14 17:48:45 +0100

    Unused import

--- src/test/java/com/amir/service/DocumentServiceImplTest.java
+++ src/test/java/com/amir/service/DocumentServiceImplTest.java
@@ -16,7 +16,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
