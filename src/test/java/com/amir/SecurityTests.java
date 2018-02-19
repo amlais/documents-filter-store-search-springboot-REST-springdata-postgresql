118f29cc8c3c6efb308a4b4182f0a8a10847f79a
Amir <amir.layes@gmail.com>
2018-02-19 17:38:58 +0100

    Oauth2 double authentification add-on

---
+++ src/test/java/com/amir/SecurityTests.java
@@ -0,0 +1,107 @@
+package com.amir;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.Base64Utils;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+//import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.amir.rest.DocumentController;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@SpringBootTest
+public class SecurityTests {
+
+	@Autowired
+	WebApplicationContext context;
+
+	@Autowired
+	private FilterChainProxy springSecurityFilterChain;
+
+	@InjectMocks
+	DocumentController controller;
+
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		mvc = MockMvcBuilders.webAppContextSetup(context)
+				.addFilter(springSecurityFilterChain).build();
+	}
+
+	@Test
+	public void docsUnauthorized() throws Exception {
+		// @formatter:off
+		mvc.perform(get("/doc")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isUnauthorized())
+				.andExpect(jsonPath("$.error", is("unauthorized")));
+		// @formatter:on
+	}
+
+	private String getAccessToken(String username, String password) throws Exception {
+		String authorization = "Basic "
+				+ new String(Base64Utils.encode("upload-client:uploadpass".getBytes()));
+		String contentType = MediaType.APPLICATION_JSON + ";charset=UTF-8";
+
+		// @formatter:off
+		String content = mvc
+				.perform(
+						post("/oauth/token")
+								.header("Authorization", authorization)
+								.contentType(
+										MediaType.APPLICATION_FORM_URLENCODED)
+								.param("username", username)
+								.param("password", password)
+								.param("grant_type", "password")
+								.param("scope", "read write")
+								.param("client_id", "upload-client")
+								.param("client_secret", "uploadpass"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(contentType))
+				.andExpect(jsonPath("$.access_token", is(notNullValue())))
+				.andExpect(jsonPath("$.token_type", is(equalTo("bearer"))))
+				.andExpect(jsonPath("$.expires_in", is(greaterThan(4000))))
+				.andExpect(jsonPath("$.scope", is(equalTo("read write"))))
+				.andReturn().getResponse().getContentAsString();
+
+		// @formatter:on
+
+		return content.substring(17, 53);
+	}
+
+	@Test
+	public void docsAuthorized() throws Exception {
+		String accessToken = getAccessToken("amir.lais", "123456");
+
+		// @formatter:off
+		mvc.perform(get("/doc/message/")
+				.header("Authorization", "Bearer " + accessToken))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.id", is(1)))
+				.andExpect(jsonPath("$.content", is("Hello REST")));
+	}
+}
