f88aab3140374e31b4d6fe13cbfbd1101888e5c8
Amir <amir.layes@gmail.com>
2018-02-19 18:33:05 +0100

    commit for test customUserdetails tests

--- src/main/java/com/amir/domain/Role.java
+++ src/main/java/com/amir/domain/Role.java
@@ -13,7 +13,7 @@ public class Role {
 	private Long id;
 	String name;
 	Role(){}
-	
+	public Role(Long id, String name){ this.id=id; this.name=name;};
 	public Role(String name){ this.name=name;}
 	public String getName(){ return name;}
 	public void setName(String name){ this.name=name;}
